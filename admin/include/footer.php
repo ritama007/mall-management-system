c4515a7c0d72efc9c825b3dad53e42a9e79422f3
KiiNG011 <kingshukroy011@gmail.com>
2023-11-13 21:24:16 +0530

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2023 Online Mall | </b> All rights reserved.
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,106 @@
+<footer id="footer" class="footer color-bg">
+      <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                     <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Online Mall</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quod delectus perferendis deleniti voluptas earum dolor cumque ratione! Optio, ex? </p>
+    
+        <div class="social-icons">
+            
+        <a href="https://www.facebook.com/rits.roy.1840" target='_blank' class='active'><i class="icon fa fa-facebook"></i></a>
+        <a href="https://x.com/RitamaChakrabo3?t=eiJHvgc1RJTwlJqIPaTlmA&s=08" target='_blank'><i class="icon fa fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/in/ritama-chakraborty-bba340222?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank'><i class="icon fa fa-linkedin"></i></a>
+        <a href="https://pin.it/6ELNgl0" target='_blank'><i class="icon fa fa-pinterest"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                     <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+    <div class="module-heading">
+        <h4 class="module-title">opening time</h4>
+    </div><!-- /.module-heading -->
+
+    <div class="module-body outer-top-xs">
+        <div class="table-responsive">
+            <table class="table">
+                <tbody>
+                    <tr><td>Monday-Friday:</td><td class="pull-right">08.00 To 18.00</td></tr>
+                    <tr><td>Saturday:</td><td class="pull-right">09.00 To 20.00</td></tr>
+                    <tr><td>Sunday:</td><td class="pull-right">10.00 To 20.00</td></tr>
+                </tbody>
+            </table>
+        </div><!-- /.table-responsive -->
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                     <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+    <div class="module-heading">
+        <h4 class="module-title">information</h4>
+    </div><!-- /.module-heading -->
+
+    <div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Kolkata, West Bengal</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(+91) 9875541946</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href = "mailto: ritamachakraborty88@gmail.com">ritamachakraborty88@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
